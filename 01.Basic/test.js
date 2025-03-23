a1829fc6e0bcb605a645728fa439e0beef6d3a6f
Jason Maxwell <max371979@gmail.com>
2025-03-23 19:47:42 -0400

    Update test.js
    equality

--- 01.Basic/test.js
+++ 01.Basic/test.js
@@ -1,6 +1,6 @@
 let firstFavNum = 37;
 let secondFavNum = 7;
 
-console.log(firstFavNum<=7);
-console.log(firstFavNum<=37);
-console.log(firstFavNum>=37);
+console.log(firstFavNum==7);
+console.log(37==37);
+console.log(8>=37);
