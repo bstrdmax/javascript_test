2f1ab20f89c9fd2be4343b50f8070739d760d2cf
Jason Maxwell <max371979@gmail.com>
2025-03-24 09:44:26 -0400

    Update test.js
    string into decimal

--- 01.Basic/test.js
+++ 01.Basic/test.js
@@ -1,9 +1,12 @@
-let money = "50";
-let money1 = 50;
+let money = "50.564";
+let money1 = 50.564;
 money=parseInt(money);
 
-money1=String(money1);
+
+money=parseFloat(money);
 
 
 console.log(typeof money);
 console.log(typeof money1);
+console.log(money);
+console.log(money1);
