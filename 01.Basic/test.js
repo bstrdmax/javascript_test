6aa9280c2a4badd3a093917cdfa174b433d55c7e
Jason Maxwell <max371979@gmail.com>
2025-03-24 09:36:18 -0400

    Update test.js
    type (string, number)

--- 01.Basic/test.js
+++ 01.Basic/test.js
@@ -1,12 +1,6 @@
-let firstName = "   jason   ";
-let secondName = "maxwell";
-let fullName = firstName.concat(secondName);
+let money = "50";
+let money1 = 50;
 
-console.log(firstName);
-console.log(fullName);
-console.log(firstName.length)
-console.log(firstName.slice(0,4));
-console.log(firstName.split(""));
-console.log(firstName.split(" ").join(" "));
-console.log(firstName.includes("j"));
-console.log(firstName.trim());
+
+console.log(typeof money);
+console.log(typeof money1);
