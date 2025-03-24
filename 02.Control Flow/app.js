31a36ee7bd9b14fbad7e351d22042a0d535591ee
Jason Maxwell <max371979@gmail.com>
2025-03-24 10:04:55 -0400

    Create app.js
    set up part 2, simple if, then , and else if

---
+++ 02.Control Flow/app.js
@@ -0,0 +1,12 @@
+let a = 15;
+let b = 10;
+
+if(a>b) {console.log("yes a is bigger");
+
+}
+    else if(b>a) {console.log("b is bigger");
+
+    }
+        else {console.log("they are equal");
+
+        };
