f4b0adcf3336cf8d48f33f9b4207b8105a1f0c19
Jason Maxwell <max371979@gmail.com>
2025-03-26 07:33:48 -0400

    Update app.js
    switch example

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,13 +1,20 @@
 
 
-let myPassword = "CatMoogl";
-let password;
+//switch(expression){case x: print break;}
 
-if(myPassword.length===8) {password = "Welcome";console.log(password);
-}
-else if(myPassword.length<=8) {password = "Paaword is too short";console.log(password);
-}
-else if (myPassword.length>=8) {password="Too Long Password, Password should be 8 characters";console.log(password);
-}
-else {password= "Please provide a password";console.log(password);
+let x=0;
+let blub;
+
+switch (x){
+    case 0:
+        bulb="off"
+        console.log(bulb);
+        break;
+    case 1:
+        blub="on"
+        console.log(bulb);
+        break;
+    default:
+        bulb="no value found";
+        console.log(bulb);
 }
