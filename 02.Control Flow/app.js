343e814f13b67793efdc44bb946869305f29f140
Jason Maxwell <max371979@gmail.com>
2025-03-26 07:56:50 -0400

    Update app.js
    for loop 1st example

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,20 +1,9 @@
+// for loop
+//DRY > don't repeat yourself
+//for(initialExpression; condition; incrementExpression) {}
 
-
-//switch(expression){case x: print break;}
-
-let fruit='orange';
-
-switch (fruit){
-    case "banana":
-        console.log("bananas are awesome");
-        break;
-    case "orange":
-        console.log("I dislike oranges, they are gross");
-        break;
-    case "apple":
-        console.log("I got her numba, how about them apples");
-        break;
-    default:
-        console.log("Never heard of that thing");
-    
+for (let i=0; i<=5; i++){
+    console.log("hey everyone",i);
 }
+
+
