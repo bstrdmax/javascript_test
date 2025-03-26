41b15e59f2c652b78c319f0d897cfb6c46bd2883
Jason Maxwell <max371979@gmail.com>
2025-03-26 07:50:27 -0400

    Update app.js

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,19 +2,19 @@
 
 //switch(expression){case x: print break;}
 
-let fruit='apple';
+let fruit='orange';
 
 switch (fruit){
     case "banana":
-        console.log("bananas are awesome")
+        console.log("bananas are awesome");
         break;
     case "orange":
-        console.log("I dislike oranges, they are gross")
+        console.log("I dislike oranges, they are gross");
         break;
     case "apple":
-        console.log("I got her numba, how about them apples")
+        console.log("I got her numba, how about them apples");
         break;
     default:
-        console.log("Never heard of that thing")
+        console.log("Never heard of that thing");
     
 }
