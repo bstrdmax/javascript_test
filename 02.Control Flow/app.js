10c69e0e91e226e49b97dea8d2999236e00b71f4
Jason Maxwell <max371979@gmail.com>
2025-03-26 07:57:45 -0400

    Update app.js
    for loop 2nd example

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,7 +2,7 @@
 //DRY > don't repeat yourself
 //for(initialExpression; condition; incrementExpression) {}
 
-for (let i=0; i<=5; i++){
+for (let i=3; i<=5; i++){
     console.log("hey everyone",i);
 }
 
