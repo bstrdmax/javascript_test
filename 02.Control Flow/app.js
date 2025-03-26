f546bdeea5032b261ffb7e0aa4ff00937b013082
Jason Maxwell <max371979@gmail.com>
2025-03-26 08:05:19 -0400

    Update app.js
    for loop 1,000 names

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,12 +2,6 @@
 //DRY > don't repeat yourself
 //for(initialExpression; condition; incrementExpression) {}
 
-for (let i=1; i<=15; i++){
-    console.log("----outer----",i);
-    //nested loop
-    for (let j=1; j<=5; j++){
-        console.log("----inner----",j);
-    }
+for (let i=0; i<=1000; i++){
+    console.log("Jason W Maxwell",i);
 }
-
-
