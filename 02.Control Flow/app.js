99fff84974f7243591ec3a0bb0757e20968c2bf9
Jason Maxwell <max371979@gmail.com>
2025-03-26 07:46:40 -0400

    Update app.js
    switch with fruits

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,31 +2,19 @@
 
 //switch(expression){case x: print break;}
 
-let day='monday';
+let fruit='apple';
 
-switch (day){
-    case "monday":
-        console.log("today is monday")
+switch (fruit){
+    case "banana":
+        console.log("bananas are awesome")
         break;
-    case "tuesday":
-        console.log("today is tuesday")
+    case "orange":
+        console.log("I dislike oranges, they are gross")
         break;
-    case "wednesday":
-        console.log("today is Wednesday")
-        break;
-    case "thursday":
-        console.log("today is thursday")
-        break;
-    case "friday":
-        console.log("today is friday")
-        break;
-    case "saturday":
-        console.log("today is saturday")
-        break;
-    case "sunday":
-        console.log("today is sunday")
+    case "apple":
+        console.log("I got her numba, how about them apples")
         break;
     default:
-        console.log("Not sure the day today")
+        console.log("Never heard of that thing")
     
 }
