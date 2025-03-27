cf2c18eef1f55355c554afaade26bd737a78765c
Jason Maxwell <max371979@gmail.com>
2025-03-27 08:26:37 -0400

    Update app.js
    access array in array my space

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,4 +1,4 @@
 //Arrays 0 index based, they start at 0 not 1
 
-const numbersArray= [1,2,3,4,5,6,7];
-console.log(numbersArray);
+const stringArray= ['eat','sleep', 'drink', 'dance'];
+console.log(stringArray [0]);
