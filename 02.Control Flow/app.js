f9be58b777c3411595aebf542a18ca1ee5fa319a
Jason Maxwell <max371979@gmail.com>
2025-03-27 08:16:07 -0400

    Update app.js
    if then with && and the !

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -4,7 +4,7 @@
 
 let password = "jason.maxwell#7";
 
-if(password.length>= 8 || password.includes("jas")) {
+if(password.length>= 8 && !password.includes("jas")) {
     console.log("Good to go");
 } else {
     console.log("Nope, try again hommie");
