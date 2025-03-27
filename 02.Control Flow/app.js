44a53572bd921cc97c318fb78c1b11e27c0c12c4
Jason Maxwell <max371979@gmail.com>
2025-03-27 08:42:04 -0400

    Update app.js
    bunch of arrays and selecting each one

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,4 +1,16 @@
 //Arrays 0 index based, they start at 0 not 1
 
-const nestArray= ['eat', [1,2,3,4],'sleep', 'drink', 'dance'];
-console.log(nestArray [1] [2]);
+const favSingers= ['josh homme', 'dave grohl','kurt cobain'];
+const favNumbers= [7, 3, 10, 37];
+const mixedArr= ['string', ['other'], 123,'true'];
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[0]);
+console.log(favNumbers[1]);
+console.log(favNumbers[2]);
+console.log(favNumbers[3]);
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
