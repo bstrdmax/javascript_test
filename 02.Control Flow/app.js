78ce62576339d1f5804b837d8117314f855fda3f
Jason Maxwell <max371979@gmail.com>
2025-03-28 15:52:07 -0400

    Update app.js
    function tester

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,12 +1,9 @@
 //fuction return
 
-function add(x,y) {
-    return x+y;
+function myFunction(m1,m2) {
+    return m1*m2;
 }
 
-const result1= add(10,20);
-const result2= add(1000,2000);
-const result3= add(10000,20000);
-console.log(result1);
-console.log(result2);
-console.log(result3);
+let x= myFunction (7,37);
+
+console.log(x);
