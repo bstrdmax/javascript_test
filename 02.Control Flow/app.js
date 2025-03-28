95fa14c9edf0efc78bd4aeb5035ef255a76c1a76
Jason Maxwell <max371979@gmail.com>
2025-03-28 08:15:52 -0400

    Update app.js
    a bunch of arrays

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,16 +1,24 @@
-//Arrays 0 index based, they start at 0 not 1
 
-const favSingers= ['josh homme', 'dave grohl','kurt cobain'];
-const favNumbers= [7, 3, 10, 37];
-const mixedArr= ['string', ['other'], 123,'true'];
-console.log(favSingers[0]);
-console.log(favSingers[1]);
-console.log(favSingers[2]);
-console.log(favNumbers[0]);
-console.log(favNumbers[1]);
-console.log(favNumbers[2]);
-console.log(favNumbers[3]);
-console.log(mixedArr[0]);
-console.log(mixedArr[1][0]);
-console.log(mixedArr[2]);
-console.log(mixedArr[3]);
+//concat();
+//includes();
+//push
+///unshift();
+//pop
+//shift();
+//sort
+//slice
+//splice
+
+const fruits = [
+    "apples",
+    "peaches",
+    "mango",
+    "stawberries",
+    "pineapple",
+    "grapefruit",
+    "pomegranate",
+    "blueberries",
+    "blackberries"
+];
+
+console.log(fruits);
