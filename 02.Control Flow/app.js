e44fdddd56934a8d7ebcdbdf691a785d6ad4d563
Jason Maxwell <max371979@gmail.com>
2025-03-28 09:01:51 -0400

    Update app.js
    bracket notation for objects

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -17,3 +17,10 @@ console.log(people.location [1]);
 console.log(people.location [2]);
 console.log(people.isProgrammer);
 console.log(people);
+
+console.log(people ['firstName']);
+console.log(people['lastName']);
+console.log(people['age']);
+console.log(people ['location']);
+console.log(people['isProgrammer']);
+console.log(people);
