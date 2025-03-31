4220617f52bc4f2021016b9ea3fdd5e87bc7bf7b
Jason Maxwell <max371979@gmail.com>
2025-03-31 11:30:25 -0400

    Update app.js
    json added

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,14 +1,22 @@
 //global and local variables
 //global
 
+
+
 function greet(){
     return `Hello my name is ${person.name} & I am ${person.age} years old`
 }
 
 const person = {
-    name:"jason W Maxwell",
+    name:"Jason W Maxwell",
     age: 46,
-    greet,
+    email: "jasonwmaxwell@proton.com",
+    isSubscribed:true,
+    hobbies: ["Writing", "Programming", "Business"],
+    address: {
+        city: "New York",
+        idk: true
+    }
 }
 
 console.log(person.greet());
