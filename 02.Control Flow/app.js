941552f21aaa56a01b87cdc4d4f5ca89080fde2f
Jason Maxwell <max371979@gmail.com>
2025-03-31 08:06:10 -0400

    Update app.js

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,9 +1,7 @@
 //function return
 
-function myFunction(m1,m2) {
-    return m1*m2;
+function sayHi(username) {
+    console.log(`Hello ${username}`);
 }
 
-let x= myFunction (7,37);
-
-console.log(x);
+sayHi("jason")
