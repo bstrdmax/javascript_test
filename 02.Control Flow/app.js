d1fe015e4c900d4aacf36f9ceebf25b9da29598b
Jason Maxwell <max371979@gmail.com>
2025-04-01 09:51:10 -0400

    Update app.js
    date

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,22 +1,7 @@
-//global and local variables
-//global
+//date
+
+const currentDate = new Date();
+console.log(currentDate);
 
 
 
-function greet(){
-    return `Hello my name is ${person.name} & I am ${person.age} years old`
-}
-
-const person = {
-    name:"Jason W Maxwell",
-    age: 46,
-    email: "jasonwmaxwell@proton.com",
-    isSubscribed:true,
-    hobbies: ["Writing", "Programming", "Business"],
-    address: {
-        city: "New York",
-        idk: true
-    }
-}
-
-console.log(person);
