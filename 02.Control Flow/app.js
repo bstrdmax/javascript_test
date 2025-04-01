4fa1bcd025fc1473fdeb12d565c270b4cc90f7b8
Jason Maxwell <max371979@gmail.com>
2025-04-01 10:04:52 -0400

    Update app.js
    logging different dates

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,12 +2,8 @@
 
 const date = new Date();
 
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDay();
-console.log(`Year: ${year}`);
-console.log(`Month: ${month}`);
-console.log(`Day: ${day}`);
-
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleTimeString());
 
 
