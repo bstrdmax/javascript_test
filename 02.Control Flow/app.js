8535373e372660265c3529874120f0c278f45f9c
Jason Maxwell <max371979@gmail.com>
2025-04-01 10:03:00 -0400

    Update app.js
    date broken up

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -3,7 +3,11 @@
 const date = new Date();
 
 const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
 console.log(`Year: ${year}`);
+console.log(`Month: ${month}`);
+console.log(`Day: ${day}`);
 
 
 
