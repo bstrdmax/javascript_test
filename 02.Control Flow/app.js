5c00f300fb419bb3cb0694b49c898d69a12046e5
Jason Maxwell <max371979@gmail.com>
2025-04-03 14:32:56 -0400

    Update app.js
    refactor in es6

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -10,5 +10,21 @@
 //greet = (username)=> `Hi ${username}`;
 //console.log(greet("Maximillion"));
 
-const double = (n) => n *2;
-console.log(double(1000));
+//const double = (n) => n *2;
+//console.log(double(1000));
+
+setTimeout(()=>{
+    console.log("Hello");
+    setTimeout(()=>{
+        console.log("Hey");
+        setTimeout(()=>{
+            console.log("Namaste");
+            setTimeout(()=>{
+                console.log("Hi");
+                setTimeout(()=>{
+                    console.log("Bonjour");
+                },2000);
+            },2000);
+        },2000);
+    },2000);
+},2000);
