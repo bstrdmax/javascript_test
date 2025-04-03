a31fb3cf307babcdbea68400ac6af68cf4ee0d16
Jason Maxwell <max371979@gmail.com>
2025-04-03 13:59:31 -0400

    Update app.js
    text using ``

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,15 +1,6 @@
-//set interval
+//strings with ``
 
-const intervalId= setInterval(function(){
-    console.log(`this function will be executed`)
-},1000)
-
-//stop interval
-
-
-setTimeout(function(){
-    clearInterval(intervalId);
-    console.log(`stop interval after 10 seconds`);
-},10000);
+console.log(`this is some
+        random stuff`);
 
 
