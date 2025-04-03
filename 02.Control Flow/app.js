dc37be04b5d902697192194e73348a8538ef763e
Jason Maxwell <max371979@gmail.com>
2025-04-03 13:52:44 -0400

    Update app.js
    interval, and stop interval

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -1,7 +1,15 @@
 //set interval
 
+const intervalId= setInterval(function(){
+    console.log(`this function will be executed`)
+},1000)
+
+//stop interval
+
+
 setTimeout(function(){
-    console.log(`this function will be executed after 3 seconds`)
-},3000)
+    clearInterval(intervalId);
+    console.log(`stop interval after 10 seconds`);
+},10000);
 
 
