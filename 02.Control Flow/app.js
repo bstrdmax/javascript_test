a274ce5d76edec91bb0b74cbf599505e6cf6f44c
Jason Maxwell <max371979@gmail.com>
2025-04-03 14:24:15 -0400

    Update app.js
    another example of shorten expression and function

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -7,5 +7,8 @@
 //console.log(greet("Maximillion"));
 
 
-greet = (username)=> `Hi ${username}`;
-console.log(greet("Maximillion"));
+//greet = (username)=> `Hi ${username}`;
+//console.log(greet("Maximillion"));
+
+const double = (n) => n *2;
+console.log(double(1000));
