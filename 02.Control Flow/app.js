6c1e7ec25c9cf89d1a6594f91bca571c9ddaef12
Jason Maxwell <max371979@gmail.com>
2025-04-03 14:08:38 -0400

    Update app.js
    using const with +

--- 02.Control Flow/app.js
+++ 02.Control Flow/app.js
@@ -2,11 +2,11 @@
 
 const firstName= "Jason";
 const lastName= "Maxwell";
-function info (){
-    return `My name is Jason and I kick the ball, SUCKA`
-}
+//function info (){
+//    return `My name is Jason and I kick the ball, SUCKA`
+//}
 
-console.log(`${info()}`);
+console.log(firstName + " " + lastName);
 
 //console.log(`${firstName} ${lastName}`);
 
